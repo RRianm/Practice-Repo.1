baf8d3d08b615932b5d5134c282a9c4ad77187cc
Rian Manocha <rianmhere@gmail.com>
2025-08-13 22:35:16 +0200

    added getapi.py file

---
+++ getapi.py
@@ -0,0 +1,32 @@
+#import requests
+
+#url = "https://pokeapi.co/api/v2/"
+#def pokeapi(name):
+  #  pokeurl = f"{url}pokemon/{name}"
+   # response = requests.get(pokeurl)
+   # print(response)
+   # if response.status_code == 200:
+   #     data = response.json()
+   #     return data
+   # else:
+    #    print("error")
+
+
+
+
+
+
+#pokemon_name = "pikachu"
+#info = pokeapi(pokemon_name)
+#if info:
+#    print(f"Name: {info["name"]}")
+#    print(f"height: {info["height"]}")
+#   print(f"id: {info["id"]}")
+#   print(f"weight: {info["weight"]}")
+
+
+import requests
+
+i
+
+
