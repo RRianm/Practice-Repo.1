2df6884e1bdbe4eafd560b5397c7a7acb4ed3358
Rian Manocha <rianmhere@gmail.com>
2025-08-14 17:52:16 +0200

    Added my own (Rian) version of accesing the Random User API as part of homework for 14/8/2025

--- getapi.py
+++ getapi.py
@@ -24,32 +24,60 @@
 #   print(f"id: {info["id"]}")
 #   print(f"weight: {info["weight"]}")
 
-#__________________________________________________________________________________-
+#__________________________________________________________________________________-e
 
+#mport requests
+
+#base_url= "https://api.freeapi.app/api/v1/public/randomusers/user/random"
+
+#def user_info():
+# url=base_url
+#  response= requests.get(url)
+#  print(response)
+
+# if response.status_code==200:
+#    data=response.json()
+#   user_data=data["data"]
+#    user_name=user_data["name"]["first"]
+#    country=user_data["location"]["country"]
+#   return user_name,country
+
+#  else:
+#    print("Failed to fetch data")
+
+#information=user_info()
+
+#if information:
+  #user_name,country=information
+  #print(f"Name: {user_name}")
+# _print(f"Country: {country}")
+
+#________________________________________________________________________
 import requests
 
-base_url= "https://api.freeapi.app/api/v1/public/randomusers/user/random"
+url = "https://api.freeapi.app/api/v1/public/randomusers/user/random"
 
-def user_info():
-  url=base_url
-  response= requests.get(url)
-  print(response)
+def getusername():
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        info = response.json()
+        user_info = info["data"]
+        username = info["data"]["name"]["first"]
+        longitude = info["data"]["location"]["coordinates"]["longitude"]
+        latitude = info["data"]["location"]["coordinates"]["latitude"]
+        return username, longitude, latitude
+    else: 
+        print("failed to access data")
 
-  if response.status_code==200:
-    data=response.json()
-    user_data=data["data"]
-    user_name=user_data["name"]["first"]
-    country=user_data["location"]["country"]
-    return user_name,country
-
-  else:
-    print("Failed to fetch data")
-
-information=user_info()
-
+information = getusername()
 if information:
-  user_name,country=information
-  print(f"Name: {user_name}")
-  print(f"Country: {country}")
+    username, longitude, latitude = information
+    print(f"Name: {username}")
+    print("Current Coordinates:     ")
+    print(f"Current longitude: {longitude}")
+    print(f"Current Latidude: {latitude}")
+
+
 
 
