26236554539cc151ca401162fa7fda315a86039f
Rian Manocha <rianmhere@gmail.com>
2025-08-14 17:20:55 +0200

    added the example api commands

--- getapi.py
+++ getapi.py
@@ -24,9 +24,32 @@
 #   print(f"id: {info["id"]}")
 #   print(f"weight: {info["weight"]}")
 
+#__________________________________________________________________________________-
 
 import requests
 
-i
+base_url= "https://api.freeapi.app/api/v1/public/randomusers/user/random"
+
+def user_info():
+  url=base_url
+  response= requests.get(url)
+  print(response)
+
+  if response.status_code==200:
+    data=response.json()
+    user_data=data["data"]
+    user_name=user_data["name"]["first"]
+    country=user_data["location"]["country"]
+    return user_name,country
+
+  else:
+    print("Failed to fetch data")
+
+information=user_info()
+
+if information:
+  user_name,country=information
+  print(f"Name: {user_name}")
+  print(f"Country: {country}")
 
 
